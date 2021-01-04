4c5f82c9a24857be8e0177c4373df1257b868d4f
Unpxre <unpxre.tw@gmail.com>
2021-01-04 14:57:10 +0800

    [add] 使用 SSID 初始化
    當未找到密碼時使用閉包傳送密碼未找到並取得密碼

--- Sources/ConnectToWifi/ConnectToWifi.swift
+++ Sources/ConnectToWifi/ConnectToWifi.swift
@@ -13,6 +13,17 @@ public struct ConnectToWifi {
     
     // MARK: Public Function
     
+    public static func bySSID(
+        _ SSID: String,
+        whenNotFindPassword getPasswordByUser: (() -> String),
+        whenConnected: ((Error?) -> Void)? = nil
+    ) {
+        var findedPassword: String? = manager.findWifiPassword(by: SSID)
+        if findedPassword == nil { findedPassword = getPasswordByUser() }
+        guard let password = findedPassword else { return }
+        bySSID(SSID, password: password)
+    }
+    
     public static func bySSID(_ SSID: String, password: String, whenConnected: ((Error?) -> Void)? = nil) {
         let configuration: NEHotspotConfiguration = .init(ssid: SSID, passphrase: password, isWEP: false)
         NEHotspotConfigurationManager.shared.apply(configuration) { error in
