6e858f4da8e4bd446a01fdad08165192ce0959b7
Unpxre <unpxre.tw@gmail.com>
2021-01-05 18:37:54 +0800

    [modify]

--- Sources/ConnectToWifi/ConnectToWifi.swift
+++ Sources/ConnectToWifi/ConnectToWifi.swift
@@ -37,7 +37,7 @@ public struct ConnectToWifi {
         NEHotspotConfigurationManager.shared.apply(configuration) { error in
             whenConnected?(error)
             guard error == nil else { return }
-            manager.save(password, on: SSID)
+            manager.update(password, on: SSID)
         }
     }
 }
