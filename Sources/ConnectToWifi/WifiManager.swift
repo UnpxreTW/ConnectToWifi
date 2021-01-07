0f10cf91b3dfcbfbde9cf5a11f7457193d657d06
Unpxre <unpxre.tw@gmail.com>
2021-01-07 13:27:03 +0800

    [test]

--- Sources/ConnectToWifi/WifiManager.swift
+++ Sources/ConnectToWifi/WifiManager.swift
@@ -28,7 +28,6 @@ public final class WifiManager {
         let query: [String: Any] = [
             kSecClass as String: kSecClassGenericPassword,
             kSecMatchLimit as String: kSecMatchLimitAll,
-            kSecReturnData as String : kCFBooleanFalse as Any,
             kSecReturnAttributes as String: true,
             kSecReturnRef as String: true
         ]
